5aa8210c44da329d844a76921539c4eb19d0bc3c
Antonidasrus <130852567+Antonidasrus@users.noreply.github.com>
2023-12-17 15:37:09 +0300

    Проблема с регистрацией

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (CustomUserViewSet, IngredientViewSet,
-                       RecipeViewSet, TagViewSet)
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 app_name = 'api'
 
@@ -10,10 +9,7 @@ router_v1 = DefaultRouter()
 router_v1.register('ingredients', IngredientViewSet, 'ingredients')
 router_v1.register('recipes', RecipeViewSet, 'recipes')
 router_v1.register('tags', TagViewSet, 'tags')
-router_v1.register('users', CustomUserViewSet, 'users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
 ]
