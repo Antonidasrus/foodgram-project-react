951c521832bee8e444a0f8c5100a371f72adbe30
Коновалов Антон Романович <akonovalov@stan-group.com>
2024-01-09 23:00:29 +0300

    4th review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
+        'HOST': os.getenv('DB_HOST', 'db',),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
